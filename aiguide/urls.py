6ca1076178b79ab6933e3a2df7475d0a1f6389fd
devipriya <devipriyam7989@gmail.com>
2025-02-25 14:49:41 +0530

    trail

--- aiguide/urls.py
+++ aiguide/urls.py
@@ -8,5 +8,7 @@ from .views import CustomLoginView
 urlpatterns = [
     path('i/', views.inde, name='inde'),  # URL for the home page with the Figma iframe 
        path('login/', CustomLoginView.as_view(), name='login'),
+       
+
 ]
 
