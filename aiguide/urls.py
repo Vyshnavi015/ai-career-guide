2beedf7517de2843dc3658dcaa26069ea0afdbcd
devipriya <devipriyam7989@gmail.com>
2025-03-08 10:11:39 +0530

    login,sign up done

--- aiguide/urls.py
+++ aiguide/urls.py
@@ -8,11 +8,6 @@ from .views import CustomLoginView
 urlpatterns = [
     path('i/', views.inde, name='inde'),  # URL for the home page with the Figma iframe 
        path('login/', CustomLoginView.as_view(), name='login'),
-<<<<<<< HEAD
-       
-
-=======
        path('signup/', views.signup, name='signup'),
->>>>>>> 0c10f707c0b69e85d6dfe0d32f41c3c1d50b32ad
 ]
 
